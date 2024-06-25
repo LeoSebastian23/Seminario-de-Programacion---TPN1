41d01f4333cae9e170c74d6026989b49ce17a8c6
marcelo montes <131314133+marcelo93Montes@users.noreply.github.com>
2024-06-25 06:00:59 -0300

    SERA ASI CHICOS?

--- src/main/java/com/proyecto/hotel/model/TarifaEstadia.java
+++ src/main/java/com/proyecto/hotel/model/TarifaEstadia.java
@@ -1,4 +1,12 @@
 package com.proyecto.hotel.model;
 
+import javax.persistence.Table;
+
+@Table
 public class TarifaEstadia {
+    private Integer total;
+    private Integer senia;
+    private Integer saldo;
+    private String estado;
+
 }
