273a9f0866a456fb68567a14614e92bd25190943
alejo <alejoparraga99@gmail.com>
2024-06-25 00:51:06 -0300

    atributos de la clase reserva

--- src/main/java/com/proyecto/hotel/model/Reserva.java
+++ src/main/java/com/proyecto/hotel/model/Reserva.java
@@ -1,4 +1,9 @@
 package com.proyecto.hotel.model;
 
 public class Reserva {
+    protected int numero;
+    protected Huesped huesped;
+    protected Habitacion habitacion;
+    protected FechaEstadia fechaEstadia;
+    protected TarifaEstadia totalEstadia;
 }
